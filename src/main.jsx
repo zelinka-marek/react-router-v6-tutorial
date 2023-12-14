d054c0916a2c83a621fe6145b42de7605d65631b
Marek Zelinka <mzelinka17@gmail.com>
2023-12-14 16:59:31 +0100

    configure future rr

--- src/main.jsx
+++ src/main.jsx
@@ -29,7 +29,16 @@ let router = createBrowserRouter([
       },
     ],
   },
-]);
+  ],
+  {
+    future: {
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_relativeSplatPath: true,
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
