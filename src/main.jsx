ded12fc7de3d5fcb21c00cdb3b1e1eca5cbe91cb
Marek Zelinka <mzelinka17@gmail.com>
2023-12-14 16:18:31 +0100

    load contacts

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, json } from "react-router-dom";
 import { ErrorPage } from "./components/error-page";
 import "./index.css";
 import Contact from "./routes/contact";
 import Root from "./routes/root";
+import * as contactsApi from "./api/contacts";
 
 let router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: async () => {
+      let contacts = await contactsApi.getContacts();
+
+      return json({ contacts });
+    },
     children: [{ path: "contacts/:contactId", element: <Contact /> }],
   },
 ]);
