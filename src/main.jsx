fac6fcd70168b978ca5c1ace1da5313e93b1f490
Marek Zelinka <mzelinka17@gmail.com>
2023-12-14 17:27:13 +0100

    nest error pages

--- src/main.jsx
+++ src/main.jsx
@@ -33,52 +33,60 @@ let router = createBrowserRouter(
         return redirect(`/contacts/${contact.id}/edit`);
       },
       children: [
-        { index: true, element: <Index /> },
         {
-          path: "contacts/:contactId",
-          element: <Contact />,
-          loader: async ({ params }) => {
-            let contact = await contactApi.getById(params.contactId);
-            if (!contact) {
-              throw json({ contact }, { status: 404, statusText: "Not Found" });
-            }
+          errorElement: <ErrorPage />,
+          children: [
+            { index: true, element: <Index /> },
+            {
+              path: "contacts/:contactId",
+              element: <Contact />,
+              loader: async ({ params }) => {
+                let contact = await contactApi.getById(params.contactId);
+                if (!contact) {
+                  throw json(
+                    { contact },
+                    { status: 404, statusText: "Not Found" }
+                  );
+                }
 
-            return json({ contact });
-          },
-          action: async ({ request, params }) => {
-            let formData = await request.formData();
-            let favorite = formData.get("favorite") === "true";
+                return json({ contact });
+              },
+              action: async ({ request, params }) => {
+                let formData = await request.formData();
+                let favorite = formData.get("favorite") === "true";
 
-            await contactApi.updateById(params.contactId, { favorite });
+                await contactApi.updateById(params.contactId, { favorite });
 
-            return json({ ok: true });
-          },
-        },
-        {
-          path: "contacts/:contactId/edit",
-          element: <EditContact />,
-          loader: async ({ params }) => {
-            let contact = await contactApi.getById(params.contactId);
+                return json({ ok: true });
+              },
+            },
+            {
+              path: "contacts/:contactId/edit",
+              element: <EditContact />,
+              loader: async ({ params }) => {
+                let contact = await contactApi.getById(params.contactId);
 
-            return json({ contact });
-          },
-          action: async ({ request, params }) => {
-            let formData = await request.formData();
-            let updates = Object.fromEntries(formData);
+                return json({ contact });
+              },
+              action: async ({ request, params }) => {
+                let formData = await request.formData();
+                let updates = Object.fromEntries(formData);
 
-            await contactApi.updateById(params.contactId, updates);
+                await contactApi.updateById(params.contactId, updates);
 
-            return redirect(`/contacts/${params.contactId}`);
-          },
-        },
-        {
-          path: "contacts/:contactId/destroy",
-          errorElement: <div>Oops! There was an error.</div>,
-          action: async ({ params }) => {
-            await contactApi.deleteById(params.contactId);
+                return redirect(`/contacts/${params.contactId}`);
+              },
+            },
+            {
+              path: "contacts/:contactId/destroy",
+              errorElement: <div>Oops! There was an error.</div>,
+              action: async ({ params }) => {
+                await contactApi.deleteById(params.contactId);
 
-            return redirect("/");
-          },
+                return redirect("/");
+              },
+            },
+          ],
         },
       ],
     },
