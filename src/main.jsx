d89c08b1084a609c3d6443f7e1e2cbe18cd8930d
Marek Zelinka <mzelinka17@gmail.com>
2023-12-14 16:13:23 +0100

    add contact page

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { ErrorPage } from "./components/error-page";
 import "./index.css";
+import Contact from "./routes/contact";
 import Root from "./routes/root";
 
 let router = createBrowserRouter([
   { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+  { path: "contacts/:contactId", element: <Contact /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
