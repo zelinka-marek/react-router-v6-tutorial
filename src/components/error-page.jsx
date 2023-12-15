bba49bfbc4f5a739348ff5c94e1e66436d4938e1
Marek Zelinka <mzelinka17@gmail.com>
2023-12-15 16:20:48 +0100

    update error page

--- src/components/error-page.jsx
+++ src/components/error-page.jsx
@@ -1,14 +1,5 @@
-import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-
-function getErrorMessage(error) {
-  if (isRouteErrorResponse(error)) {
-    return error.statusText;
-  } else if (error instanceof Error) {
-    return error.message;
-  }
-
-  return "Unknown error";
-}
+import { useRouteError } from "react-router-dom";
+import { getErrorMessage } from "../utils/misc.js";
 
 export function ErrorPage() {
   let error = useRouteError();
@@ -16,10 +7,12 @@ export function ErrorPage() {
   console.error(errorMessage);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <div className="text-center">
+      <h1 className="text-2xl font-bold text-gray-900">Oops!</h1>
+      <p className="mt-2 text-sm text-gray-500">
+        Sorry, an unexpected error has occurred.
+      </p>
+      <p className="mt-6 text-sm text-gray-500">
         <i>{errorMessage}</i>
       </p>
     </div>
--- src/utils/misc.js
+++ src/utils/misc.js
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge";
 export function classNames(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function getErrorMessage(error) {
+  if (typeof error === "string") {
+    return error;
+  }
+
+  if (
+    error &&
+    typeof error === "object" &&
+    "message" in error &&
+    typeof error.message === "string"
+  ) {
+    return error.message;
+  }
+
+  console.error("Unable to get error message for error", error);
+
+  return "Unknown Error";
+}
