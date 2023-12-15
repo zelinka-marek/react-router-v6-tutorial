239f50686ab60db40fd9a9755f07da49f1a78c45
Marek Zelinka <mzelinka17@gmail.com>
2023-12-15 13:30:00 +0100

    update edit form

--- src/components/forms.jsx
+++ src/components/forms.jsx
@@ -26,3 +26,17 @@ export let Input = forwardRef(({ className, ...props }, ref) => {
   );
 });
 Input.displayName = "Input";
+
+export let TextArea = forwardRef(({ className, ...props }, ref) => {
+  return (
+    <textarea
+      ref={ref}
+      {...props}
+      className={classNames(
+        "block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 aria-[invalid]:text-red-900 aria-[invalid]:ring-red-300 aria-[invalid]:placeholder:text-red-300 aria-[invalid]:focus:ring-red-500 sm:text-sm/6",
+        className,
+      )}
+    />
+  );
+});
+TextArea.displayName = "TextArea";
--- src/routes/edit-contact.jsx
+++ src/routes/edit-contact.jsx
@@ -1,4 +1,5 @@
 import { Form, useLoaderData, useNavigate } from "react-router-dom";
+import { Input, Label, TextArea } from "../components/forms";
 
 export default function EditContact() {
   const { contact } = useLoaderData();
@@ -9,53 +10,95 @@ export default function EditContact() {
   }
 
   return (
-    <Form method="post" id="contact-form">
-      <p>
-        <span>Name</span>
-        <input
-          placeholder="First"
-          aria-label="First name"
-          type="text"
-          name="first"
-          defaultValue={contact.first}
-        />
-        <input
-          placeholder="Last"
-          aria-label="Last name"
-          type="text"
-          name="last"
-          defaultValue={contact.last}
-        />
-      </p>
-      <label>
-        <span>Twitter</span>
-        <input
-          type="text"
-          name="twitter"
-          placeholder="@jack"
-          defaultValue={contact.twitter}
-        />
-      </label>
-      <label>
-        <span>Avatar URL</span>
-        <input
-          placeholder="https://example.com/avatar.jpg"
-          aria-label="Avatar URL"
-          type="text"
-          name="avatar"
-          defaultValue={contact.avatar}
-        />
-      </label>
-      <label>
-        <span>Notes</span>
-        <textarea name="notes" defaultValue={contact.notes} rows={6} />
-      </label>
-      <p>
-        <button type="submit">Save</button>
-        <button type="button" onClick={goBack}>
-          Cancel
-        </button>
-      </p>
+    <Form method="post" className="space-y-6">
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <Label htmlFor="first" className="sm:pt-1.5">
+          First name
+        </Label>
+        <div className="max-sm:mt-2 sm:col-span-2">
+          <Input
+            type="text"
+            name="first"
+            id="first"
+            defaultValue={contact.first}
+            className="sm:max-w-xs"
+          />
+        </div>
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <Label htmlFor="last" className="sm:pt-1.5">
+          Last name
+        </Label>
+        <div className="max-sm:mt-2 sm:col-span-2">
+          <Input
+            type="text"
+            name="last"
+            id="last"
+            defaultValue={contact.last}
+            className="sm:max-w-xs"
+          />
+        </div>
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <Label htmlFor="twitter" className="sm:pt-1.5">
+          Twitter
+        </Label>
+        <div className="max-sm:mt-2 sm:col-span-2">
+          <Input
+            type="text"
+            name="twitter"
+            id="twitter"
+            defaultValue={contact.twitter}
+            className="sm:max-w-xs"
+            aria-label="Twitter username"
+          />
+        </div>
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <Label htmlFor="avatar" className="sm:pt-1.5">
+          Avatar
+        </Label>
+        <div className="max-sm:mt-2 sm:col-span-2">
+          <Input
+            type="url"
+            name="avatar"
+            id="avatar"
+            defaultValue={contact.avatar}
+            className="sm:max-w-sm"
+            placeholder="https://example.com/avatar.jpg"
+          />
+        </div>
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <Label htmlFor="notes" className="sm:pt-1.5">
+          Notes
+        </Label>
+        <div className="max-sm:mt-2 sm:col-span-2">
+          <TextArea
+            name="notes"
+            id="notes"
+            defaultValue={contact.notes}
+            rows={6}
+          />
+        </div>
+      </div>
+      <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4">
+        <div className="flex items-center justify-end gap-x-6 sm:col-start-2 sm:flex-row-reverse">
+          <button
+            type="button"
+            onClick={goBack}
+            className="text-sm/6 font-semibold text-gray-900"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="inline-flex justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+          >
+            Save
+          </button>
+        </div>
+      </div>
     </Form>
   );
 }
