6accea421591702be879aa6a6cac0386d688af78
Marek Zelinka <mzelinka17@gmail.com>
2023-12-15 12:18:56 +0100

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
+import formsPlugin from "@tailwindcss/forms";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        gray: colors.slate,
+      },
+    },
+  },
+  plugins: [formsPlugin],
+};
