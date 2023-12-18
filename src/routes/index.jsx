9e145164c617c114c6c213f1d3a9324f54493e55
Marek Zelinka <mzelinka17@gmail.com>
2023-12-18 17:28:28 +0100

    fix index page form

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -8,7 +8,7 @@ export default function Index() {
       title="No contact selected"
       description="Select a contact on the left or create a new contact."
     >
-      <Form method="post">
+      <Form method="post" action="/">
         <button
           type="submit"
           className="flex items-center gap-1 rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
